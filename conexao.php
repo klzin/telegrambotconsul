973c5498a35d994b7c5f8fa25b56a31f9cd39265
klzin <68357260+klzin@users.noreply.github.com>
2022-09-11 23:04:08 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'bot');
+define('HOST', 'sql10.freesqldatabase.com');
+define('USUARIO', 'sql10518930');
+define('SENHA', 'TABmzn3Ts9');
+define('DB', 'sql10518930');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB);
 
